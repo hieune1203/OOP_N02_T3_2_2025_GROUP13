6273a42780c9060c82bb2b703e04e4ad8da7230d
PHAN TRỌNG HIẾU <24100082@st.phenikaa-uni.edu.vn>
2025-08-07 18:48:14 +0000

    QUANLIBANAO

--- complete/src/main/java/com/example/servingwebcontent/controller/GiaoDichController.java
+++ complete/src/main/java/com/example/servingwebcontent/controller/GiaoDichController.java
@@ -1,3 +1,5 @@
+package com.example.servingwebcontent.controller;
+
 import database.DanhSachGiaoDich;
 import model.GiaoDich;
 
--- complete/src/main/java/com/example/servingwebcontent/database/DanhSachGiaoDich.java
+++ complete/src/main/java/com/example/servingwebcontent/database/DanhSachGiaoDich.java
@@ -1,3 +1,5 @@
+package database;
+
 import model.GiaoDich;
 
 import java.util.ArrayList;
