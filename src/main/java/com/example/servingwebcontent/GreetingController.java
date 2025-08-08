51b4597d4ec0e632c5fd33fba8f138e56ce0be3d
PHAN TRỌNG HIẾU <24100082@st.phenikaa-uni.edu.vn>
2025-08-08 09:16:51 +0000

    hh

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -3,15 +3,13 @@ package com.example.servingwebcontent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
-
+    @GetMapping("/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("message", "Welcome to the web interface!");
+        return "greeting";
+    }
 }
